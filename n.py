75bfa44a1f2db578550aca0ff10e20e044d165ab
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-05-03 13:16:59 +0630

    Update n.py

--- n.py
+++ n.py
@@ -11,7 +11,7 @@ import requests
 from datetime import datetime
 
 # Colors
-w, g, y, r_clr, b = "\033[1;00m", "\033[1;32m", "\033[1;33m", "\033[1;34m"
+w, g, y, r_clr, b = "\033[1;00m", "\033[1;32m", "\033[1;33m", "\033[1;31m", "\033[1;34m
 
 # [ CONFIGURATION ]
 DEFAULT_GATEWAY = "192.168.110.1"
