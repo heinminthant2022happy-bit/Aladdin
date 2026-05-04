d23311aaec3ac2e300071b3ff2325ef53e1967ca
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-05-04 07:12:33 +0630

    Update go.py

--- go.py
+++ go.py
@@ -1,3 +1,12 @@
 import lack
-lack.main()
+import os
 
+if __name__ == "__main__":
+    try:
+        # .so ဖိုင်ထဲက main function ကို လှမ်းခေါ်တာပါ
+        lack.main()
+    except AttributeError:
+        print("Error: main() function ကို ရှာမတွေ့ပါဘူး။")
+    except Exception as e:
+        print(f"Error: {e}")
+      
