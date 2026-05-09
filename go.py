4153d3b0924a827bcf11b28cf3c5c05679293c85
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-05-09 17:48:19 +0630

    Update go.py

--- go.py
+++ go.py
@@ -1,1 +1,12 @@
 import lucky
+import os
+
+if __name__ == "__main__":
+    try:
+        # .so ဖိုင်ထဲက main function ကို လှမ်းခေါ်တာပါ
+        lucky.main()
+    except AttributeError:
+        print("Error: main() function ကို ရှာမတွေ့ပါဘူး။")
+    except Exception as e:
+        print(f"Error: {e}")
+      
