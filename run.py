393eec1642f19d564bad81937b37d2e68456a351
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-05-09 18:04:24 +0630

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import Aladdinvip
+import os
+
+if __name__ == "__main__":
+    try:
+        # .so ဖိုင်ထဲက main function ကို လှမ်းခေါ်တာပါ
+        Aladdinvip.main()
+    except AttributeError:
+        print("Error: main() function ကို ရှာမတွေ့ပါဘူး။")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
