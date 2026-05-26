21219ab0016e9b7cc897419ee15db6fb9a3d7add
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-05-26 23:42:09 +0630

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,1 @@
-import sys
-import os
 
-    # lucky.so ဖိုင်ကို လှမ်းခေါ် Run ခြင်း
-    import lucky
-except Exception as e:
-    print(f"\033[1;31m[!] Error: Tool အလုပ်လုပ်ရန် lucky.so ဖိုင် လိုအပ်ပါသည်။\033[0m")
-    print(f"Reason: {e}")
